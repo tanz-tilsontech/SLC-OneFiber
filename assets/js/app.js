799bf1eb808cd72f0e5c87cc6775426cf5ff0ac1
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-20 13:58:52 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -352,6 +352,7 @@ var featureLayer = L.geoJson(null, {
         legendItems[feature.properties.Status] = feature.properties["marker-color"];
       }
       content += "<table>";
+      if (feature.properties) {
       layer.on({
         click: function (e) {
           identifyFeature(L.stamp(layer));
@@ -372,6 +373,28 @@ var featureLayer = L.geoJson(null, {
   }
 });
 
+
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      layer.on({
+        click: function (e) {
+          identifyFeature(L.stamp(layer));
+          highlightLayer.clearLayers();
+          highlightLayer.addData(featureLayer.getLayer(L.stamp(layer)).toGeoJSON());
+        },
+        mouseover: function (e) {
+          if (config.hoverProperty) {
+            $(".info-control").html(feature.properties[config.hoverProperty]);
+            $(".info-control").show();
+          }
+        },
+        mouseout: function (e) {
+          $(".info-control").hide();
+        }
+      });
+    }
+  }
+});
 
 // Fetch the GeoJSON file
 $.getJSON(config.geojson, function (data) {
