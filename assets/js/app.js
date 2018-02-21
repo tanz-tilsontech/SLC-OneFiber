a64fe1ac0fc37e7a008ed0b123218552bab97a68
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-21 16:12:27 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -237,25 +237,9 @@ function drawCharts() {
 
   // WPID
   $(function() {
-    var result = alasql("SELECT wpid AS label, SUM(proposed_footage::NUMBER) AS footage FROM ? GROUP BY wpid", [features]);
+    var result = alasql("SELECT wpid AS label, SUM(COALESCE(cable_placement_total_footage_cx_final::NUMBER)) AS footage FROM ? GROUP BY wpid", [features]);
     var columns = $.map(result, function(proposed_footage) {
       return [[proposed_footage.label, proposed_footage.footage]];
-    });
-    var chart = c3.generate({
-        bindto: "#wpid-chart",
-        data: {
-          type: "bar",
-          columns: columns
-        }
-    });
-  });
-
-
-  // WPID
-  $(function() {
-    var result = alasql("SELECT wpid AS label, SUM(construction_footage_cx_final::NUMBER) AS footage FROM ? GROUP BY wpid", [features]);
-    var columns = $.map(result, function(construction_footage_cx_final) {
-      return [[construction_footage_cx_final.label, construction_footage_cx_final.footage]];
     });
     var chart = c3.generate({
         bindto: "#wpid-chart",
