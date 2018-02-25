370e916c40a39c15370fb29c519874eee6de9fe4
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-24 20:48:26 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -327,11 +327,16 @@ function buildConfig() {
       ].join("");
     },
     events: {
-      "click .zoom": function (e, value, row, index, layer) {
-        layer.feature.geometry.type === "Point"
-        map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 19);
-        highlightLayer.clearLayers();
-        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
+      "click .zoom": function (e, value, row, index) {
+        function zoomToFeature(id) {
+          featureLayer.eachLayer(function (layer) {
+            if (layer.feature.properties["Fulcrum Id"] == id) {
+              map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 18);
+              highlightLayer.clearLayers();
+              highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
+            }
+          });
+        }
       },
       "click .identify": function (e, value, row, index) {
         identifyFeature(row.leaflet_stamp);
