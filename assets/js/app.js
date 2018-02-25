424c03bb41106606b8802a6c95e8311d0be51db0
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-24 21:48:59 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -485,19 +485,10 @@ var legend = L.control({position: 'bottomleft'});
 
 legend.onAdd = function (map) {
 
-    var div = L.DomUtil.create('div', 'legend'),
-        grades = [58, 72, 90, 100, 114, 128],
-        labels = [];
-    div.innerHTML += '<b>Site Stem Density<br>(Trees/HA)</b><br>';
-    
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length - 1; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + '&ndash;' + grades[i + 1] + '<br>' ;
-    }
+  var div = L.DomUtil.create('div', 'legend'),
+  div.innerHTML = "<a<img src='assets/img/fulcrum-power.png'></a>";
 
-    return div;
+  return div;
 };
 
 legend.addTo(map);
