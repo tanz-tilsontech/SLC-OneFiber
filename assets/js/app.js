88928268b6dac3fdfe4c29716e750247a7a5ec54
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-25 20:26:17 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -216,17 +216,6 @@ var properties = [{
   }
 },
 {
-  value: "locates_cx_url",
-  label: "Construction Locates",
-  table: {
-    visible: true,
-    sortable: true
-  },
-  filter: {
-    type: "string"
-  }
-},
-{
   value: "cable_placement_start_date_cx_final",
   label: "Cable Placement Start Date",
   table: {
@@ -473,21 +462,6 @@ var featureLayer = L.geoJson(null, {
   },
   onEachFeature: function (feature, layer) {
     if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>";
-      $.each(feature.properties, function(index, prop) {
-        if (prop === null) {
-          prop = "";
-        } else if (prop.toString().indexOf("https://web.fulcrumapp.com/shares/fb96b48deb5cfb94/photos/") === 0) {
-          prop = "<a href='#' onclick='photoGallery(\"" + prop + "\"); return false;'>View Photos</a>";
-        } else if (prop.toString().indexOf("https://web.fulcrumapp.com/shares/fb96b48deb5cfb94/videos/") === 0) {
-          prop = "<a href='" + prop + "' target='blank'>View videos</a>";
-        } else if (prop.toString().indexOf("https://web.fulcrumapp.com/shares/fb96b48deb5cfb94/signatures/") === 0) {
-          prop = "<a href='" + prop + "' target='blank'>View signatures</a>";
-        } else if (prop.toString().indexOf("https://") === 0 || prop.toString().indexOf("http://") === 0) {
-          prop = "<a href='" + prop + "' target='blank'>" + prop + "</a>";
-        }
-        content += "<tr><th>" + index + "</th><td>" + prop + "</td></tr>";
-      });
       layer.on({
         click: function (e) {
           identifyFeature(L.stamp(layer));
@@ -516,25 +490,6 @@ var featureLayer = L.geoJson(null, {
     }
   }
 });
-
-
-function photoGallery(photos) {
-  var photoArray = [];
-  var photoIDs = photos.split("photos=")[1];
-  $.each(photoIDs.split("%2C"), function(index, id) {
-    photoArray.push({href: "https://web.fulcrumapp.com/shares/fb96b48deb5cfb94/photos/" + id});
-  });
-  $.fancybox(photoArray, {
-    "type": "image",
-    "showNavArrows": true,
-    "padding": 0,
-    "scrolling": "no",
-    beforeShow: function () {
-      this.title = "Photo " + (this.index + 1) + " of " + this.group.length + (this.title ? " - " + this.title : "");
-    }
-  });
-  return false;
-}
 
 
 // Fetch the GeoJSON file
