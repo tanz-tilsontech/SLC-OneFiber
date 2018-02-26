57e2a30869967267ae748591a8e7f1d73a1dc7d9
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-26 09:58:52 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -762,49 +762,67 @@ fulcrumControl.onAdd = function (map) {
 map.addControl(fulcrumControl);
 
 
-var Core = L.Control.LinearCore({ onSelect: function(e){ if(!e.total){ return; }
-  var distance = e.total.scalar;
+L.control.polylineMeasure(options).addTo(map);
 
-  if(e.total.unit === 'mi'){
-    distance *= e.sub_unit;
-  } else if(e.total.unit === 'km'){
-    distance *= 3280.84;
-  } else if(e.total.unit === 'm'){
-    distance *= 3.28084;
-  }
-
-  var days_underground = 350,
-      days_above_ground = 900,
-      html = [
-          '<table>',
-          ' <tr><td class="cost_label">Days Underground Build:</td><td class="cost_value">${days_underground}</td></tr>',
-          ' <tr><td class="cost_label">Days Aerial Build:</td><td class="cost_value">${days_above_ground}</td></tr>',
-          '</table>'
-      ].join(''),
-      numberWithCommas = function(x) {
-          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      };
-
-  var data = {
-      total_days_underground: numberWithCommas(L.Util.formatNum(distance / days_underground, 2)),
-      total_days_above_ground: numberWithCommas(L.Util.formatNum(distance / days_above_ground, 2))
-  };
-
-  if(e.rulerOn){
-      var content = L.Util.template(html, data),
-          popup = L.popup().setContent(content);
-
-      e.total_label.bindPopup(popup, { offset: [45, 0] });
-      e.total_label.openPopup();
-  }
-}
-});
-
-map.addControl(new Core({
-  unitSystem: 'imperial',
-  color: '#FF0080',
-  type: 'line'
-}));
+options = {
+    bearingTextIn: 'In'                     // language dependend label for inbound bearings
+    bearingTextOut: 'Out',                  // language dependend label for outbound bearings
+    bearingTextOut: 'Out',                  // language dependend label for outbound bearings
+    bindTooltipText: "Click and drag to <b>move point</b><br>Press CTRL-key and click to <b>resume line</b>",
+                                            // language dependend label for last point's tooltip
+    changeUnitsText: 'Change Units',        // language dependend label for "Change units"-button
+    position: 'topleft',                    // Position to show the control. Possible values are: 'topright', 'topleft', 'bottomright', 'bottomleft'
+    unit: 'imperial',                         // Show imperial or metric distances. Values: 'metres', 'landmiles', 'nauticalmiles'
+    showBearings: false,                    // Whether bearings are displayed within the tooltips
+    measureControlTitleOn: 'Turn on PolylineMeasure',   // Title for the control going to be switched on
+    measureControlTitleOff: 'Turn off PolylineMeasure', // Title for the control going to be switched off
+    measureControlLabel: '&#8614;',         // HTML to place inside the control
+    measureControlClasses: [],              // Classes to apply to the control
+    backgroundColor: '#8f8',                // Background color for control when selected
+    cursor: 'crosshair',                    // Cursor type to show when creating measurements
+    clearMeasurementsOnStop: true,          // Clear all the measurements when the control is unselected
+    showMeasurementsClearControl: false,    // Show a control to clear all the measurements
+    clearControlTitle: 'Clear Measurements',// Title text to show on the clear measurements control button
+    clearControlLabel: '&times',            // Clear control inner html
+    clearControlClasses: [],                // Collection of classes to add to clear control button
+    showUnitControl: false,                 // Show a control to change the units of measurements
+    tempLine: {                             // Styling settings for the temporary dashed line
+        color: '#00f',                      // Dashed line color
+        weight: 2                           // Dashed line weight
+    },          
+    fixedLine: {                            // Styling for the solid line
+        color: '#006',                      // Solid line color
+        weight: 2                           // Solid line weight
+    },
+    startCircle: {                          // Style settings for circle marker indicating the starting point of the polyline
+        color: '#000',                      // Color of the border of the circle
+        weight: 1,                          // Weight of the circle
+        fillColor: '#0f0',                  // Fill color of the circle
+        fillOpacity: 1,                     // Fill opacity of the circle
+        radius: 3                           // Radius of the circle
+    },
+    intermedCircle: {                       // Style settings for all circle markers between startCircle and endCircle
+        color: '#000',                      // Color of the border of the circle
+        weight: 1,                          // Weight of the circle
+        fillColor: '#ff0',                  // Fill color of the circle
+        fillOpacity: 1,                     // Fill opacity of the circle
+        radius: 3                           // Radius of the circle
+    },
+    currentCircle: {                        // Style settings for circle marker indicating the latest point of the polyline during drawing a line
+        color: '#000',                      // Color of the border of the circle
+        weight: 1,                          // Weight of the circle
+        fillColor: '#f0f',                  // Fill color of the circle
+        fillOpacity: 1,                     // Fill opacity of the circle
+        radius: 3                           // Radius of the circle
+    },
+    endCircle: {                            // Style settings for circle marker indicating the last point of the polyline
+        color: '#000',                      // Color of the border of the circle
+        weight: 1,                          // Weight of the circle
+        fillColor: '#f00',                  // Fill color of the circle
+        fillOpacity: 1,                     // Fill opacity of the circle
+        radius: 3                           // Radius of the circle
+    },
+};
 
 
 $("#refresh-btn").click(function() {
