62210bc0e0742857bf7d844e3db47d2c901b8f13
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-27 10:15:31 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -300,7 +300,7 @@ function drawCharts() {
   $(function() {
     var result = alasql("SELECT status AS label1, hub AS label2, COUNT(*) AS total FROM ? GROUP BY hub", [features]);
     var columns = $.map(result, function(status) {
-      return [[status.label1, status.label2, status.total]];
+      return [[status.label1, status.total]];
     });
     var chart = c3.generate({
         bindto: "#hub-status-chart",
