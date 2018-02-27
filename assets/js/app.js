dd1c737b0a2c2781b9239517b286ee3ee906a7f0
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-27 10:24:37 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -766,6 +766,17 @@ fulcrumControl.onAdd = function (map) {
 map.addControl(fulcrumControl);
 
 
+var fulcrumControl1 = new L.control({
+  position: "bottomleft"
+});
+fulcrumControl1.onAdd = function (map) {
+  var div = L.DomUtil.create("div");
+  div.innerHTML = "<a target='_blank'><img src=\"assets/pictures/tilson-logo.png\" width=\"120px\" height=\"160px\"></a>";
+  return div;
+};
+map.addControl(fulcrumControl1);
+
+
 $("#refresh-btn").click(function() {
   featureLayer.clearLayers();
   map.setView([40.5912,-111.837],9)
