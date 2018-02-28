55eb3ef55e8aaab935bd5e22c9a350bf9c4d2bee
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-27 20:42:25 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -531,7 +531,9 @@ $.getJSON(config.geojson, function (data) {
         $("#legend").append("<p><i style='background:" + value + "'></i> " + property + "</p>");
       }
     });
+    $("#table").bootstrapTable("load", JSON.parse(JSON.stringify(tableFeatures)));
     $.each(tableFeatures, function(index, value) {
+      tableFeatures = [];
       if (value.field == style.property) {
         tableFeatures[index].cellStyle = function cellStyle(value, row, index, field) {
           if (style.values[row[style.property]] && style.values[row[style.property]].startsWith("http")) {
