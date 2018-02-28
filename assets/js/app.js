5ef112310bc59b72f3be7845cc4a8151438b5fc3
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-27 19:24:48 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -688,7 +688,7 @@ function identifyFeature(id) {
     if (!value) {
       value = "";
     }
-    if (value.indexOf("edit") === 0 && (value.indexOf("http") === 0 || value.indexOf("https") === 0)) {
+    if (typeof value == "string" && (value.indexOf("edit-record") === 0 && (value.indexOf("http") === 0 || value.indexOf("https") === 0)) {
       value = "<a href='" + value + "' target='_blank'>" + "Edit Record" + "</a>";
     }
     $.each(properties, function(index, property) {
