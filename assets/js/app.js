63d7145827899c82eda6c70a15906fc842363436
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-02-28 21:07:03 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -265,7 +265,7 @@ var properties = [{
 function drawCharts() {
   // HUB COMPLETE
   $(function() {
-    var result = alasql("SELECT hub AS label, COUNT(CASE WHEN cable_placement_pass_date_qc_final IS NOT NULL) AS total FROM ? GROUP BY hub", [features]);
+    var result = alasql("SELECT hub AS label, COUNT(cable_placement_pass_date_qc_final) AS total FROM ? GROUP BY hub", [features]);
     var columns = $.map(result, function(status) {
       return [[status.label, status.total]];
     });
