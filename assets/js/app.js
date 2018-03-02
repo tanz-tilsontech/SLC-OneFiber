5f5bccd1d437a9036659a34c0a55e237d37d138f
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-03-01 19:38:38 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -812,6 +812,80 @@ L.easyPrint({
 }).addTo(map)
 
 
+//Edit 'key' and 'columns' to connect your spreadsheet
+
+//enter google sheets key here
+var key =
+  "https://docs.google.com/spreadsheets/d/1yNyg2grJYCICN0g_UkYuQXtTEZ_EFBVtXv--r9tc6oI/edit?usp=sharing";
+
+//"data" refers to the column name with no spaces and no capitals
+//punctuation or numbers in your column name
+//"title" is the column name you want to appear in the published table
+var columns = [{
+  "data": "hub",
+  "title": "HUB"
+}, {
+  "data": "nfid",
+  "title": "SITE NFID"
+}, {
+  "data": "site",
+  "title": "SITE NAME"
+}, {
+  "data": "start_date",
+  "title": "START DATE"
+}, {
+  "data": "complete_date",
+  "title": "COMPLETE DATE"
+}, {
+  "data": "cable_footage",
+  "title": "CABLE FOOTAGE"
+}, {
+  "data": "complete_percent",
+  "title": "COMPLETE PERCENT"
+}];
+
+$(document).ready(function() {
+
+  function initializeTabletopObject() {
+    Tabletop.init({
+      key: key,
+      callback: function(data, tabletop) {
+        writeTable(data); //call up datatables function
+      },
+      simpleSheet: true,
+      debug: false
+    });
+  }
+
+  initializeTabletopObject();
+
+  function writeTable(data) {
+    //select main div and put a table there
+    //use bootstrap css to customize table style: http://getbootstrap.com/css/#tables
+    $('#graphic').html(
+      '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-condensed table-responsive" id="mySelection"></table>'
+    );
+
+    //initialize the DataTable object and put settings in
+    $("#mySelection").DataTable({
+      "autoWidth": false,
+      "data": data,
+      "columns": columns,
+      "order": [
+        [2, "desc"]
+      ], //order on second column
+      "pagingType": "simple" //no page numbers
+        //uncomment these options to simplify your table
+        //"paging": false,
+        //"searching": false,
+        //"info": false
+    });
+  }
+});
+//end of writeTable
+
+
+
 $("#refresh-btn").click(function() {
   featureLayer.clearLayers();
   map.setView([40.5912,-111.837],9)
@@ -852,9 +926,11 @@ $("#chart-btn").click(function() {
 });
 
 $("#sheets-btn").click(function() {
-  var win = window.open("https://docs.google.com/spreadsheets/d/1yNyg2grJYCICN0g_UkYuQXtTEZ_EFBVtXv--r9tc6oI/edit?usp=sharing", '_blank');
-  win.focus();
+  $("#sheets-modal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
 });
+
 
 $("#view-sql-btn").click(function() {
   alert($("#query-builder").queryBuilder("getSQL", false, false).sql);
