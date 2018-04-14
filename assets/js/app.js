f8a0719b05a5e431f56256ec0d73e6c093458ee4
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-04-13 21:16:06 -0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,74 @@
+
+checkAuth();
+bindUIActions();
+
+
+function bindUIActions() {
+  $("#login-btn").click(function() {
+    login();
+  });
+
+  $("#logout-btn").click(function() {
+    logout();
+  });
+
+  $("#login-modal").on("shown.bs.modal", function (e) {
+    $(".modal-backdrop").css("opacity", "1");
+  });
+
+  $("#login-modal").on("hidden.bs.modal", function (e) {
+    $(".modal-backdrop").css("opacity", "");
+  });
+};
+
+
+function checkAuth() {
+  if (!localStorage.getItem("fulcrum_app_token")) {
+    $("#login-modal").modal("show");
+  } else {
+    $("#login-modal").modal("hide");
+  }
+};
+
+function login() {
+  var username = $("#email").val();
+  var password = $("#password").val();
+  $.ajax({
+    type: "GET",
+    url: "https://api.fulcrumapp.com/api/v2/users.json",
+    contentType: "application/json",
+    dataType: "json",
+    headers: {
+      "Authorization": "Basic " + btoa(username + ":" + password)
+    },
+    statusCode: {
+      401: function() {
+        alert("Incorrect credentials, please try again.");
+      }
+    },
+    success: function (data) {
+      $.each(data.user.contexts, function(index, context) {
+        if (context.name == "Tilson SLC") {
+          localStorage.setItem("fulcrum_app_token", btoa(context.api_token));
+          localStorage.setItem("fulcrum_userfullname", data.user.first_name + " " + data.user.last_name);
+        }
+      });
+      if (!localStorage.getItem("fulcrum_app_token")) {
+        alert("This login does not have access to the Fulcrum Labs organization.");
+      }
+      checkAuth();
+    }
+  });
+};
+
+function logout() {
+  localStorage.removeItem("fulcrum_app_token");
+  localStorage.removeItem("fulcrum_userfullname");
+  location.reload();
+};
+
+
+
 // Configuration of Routes in Fulcrum
 
 var config = {
