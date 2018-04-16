970541bb325a43e3ba6012e8234a48d4e3d5d39c
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-04-16 15:36:58 -0600

    asdf

--- assets/js/app.js
+++ assets/js/app.js
@@ -518,6 +518,96 @@ $(function() {
 
 
 
+
+function buildConfig() {
+  filters = [];
+  table = [{
+    field: "action",
+    title: "<i class='fa fa-gear'></i>&nbsp;Action",
+    align: "center",
+    valign: "middle",
+    width: "75px",
+    cardVisible: false,
+    switchable: false,
+    formatter: function(value, row, index) {
+      return [
+        '<a class="zoom" href="javascript:void(0)" title="Zoom" style="margin-right: 10px;">',
+          '<i class="fa fa-search-plus"></i>',
+        '</a>',
+        '<a class="identify" href="javascript:void(0)" title="Identify" style="margin-right: 10px;">',
+          '<i class="fa fa-info-circle"></i>',
+        '</a>'
+      ].join("");
+    },
+    events: {
+      "click .zoom": function (e, value, row, index) {
+        var layer = featureLayer.getLayer(row.leaflet_stamp);
+        map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 19);
+        highlightLayer.clearLayers();
+        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
+      },
+      "click .identify": function (e, value, row, index) {
+        identifyFeature(row.leaflet_stamp);
+        highlightLayer.clearLayers();
+        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
+      }
+    }
+  }];
+
+  $.each(properties, function(index, value) {
+    // Filter config
+    if (value.filter) {
+      var id;
+      if (value.filter.type == "integer") {
+        id = "cast(properties->"+ value.value +" as int)";
+      }
+      else if (value.filter.type == "double") {
+        id = "cast(properties->"+ value.value +" as double)";
+      }
+      else {
+        id = "properties->" + value.value;
+      }
+      filters.push({
+        id: id,
+        label: value.label
+      });
+      $.each(value.filter, function(key, val) {
+        if (filters[index]) {
+          // If values array is empty, fetch all distinct values
+          if (key == "values" && val.length === 0) {
+            alasql("SELECT DISTINCT(properties->"+value.value+") AS field FROM ? ORDER BY field ASC", [geojson.features], function(results){
+              distinctValues = [];
+              $.each(results, function(index, value) {
+                distinctValues.push(value.field);
+              });
+            });
+            filters[index].values = distinctValues;
+          } else {
+            filters[index][key] = val;
+          }
+        }
+      });
+    }
+    // Table config
+    if (value.table) {
+      table.push({
+        field: value.value,
+        title: value.label
+      });
+      $.each(value.table, function(key, val) {
+        if (table[index+1]) {
+          table[index+1][key] = val;
+        }
+      });
+    }
+  });
+
+  buildFilters();
+  buildTable();
+};
+
+
+
 // Basemap Layers
 var mapboxOSM = L.tileLayer('http://{s}.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWNvdHJ1c3QiLCJhIjoibGo4TG5nOCJ9.QJnT2dgjL4_4EA7WlK8Zkw', {
     maxZoom: 20
@@ -806,95 +896,6 @@ $.getJSON(config1.geojson, function (data) {
 
 
 
-function buildConfig() {
-  filters = [];
-  table = [{
-    field: "action",
-    title: "<i class='fa fa-gear'></i>&nbsp;Action",
-    align: "center",
-    valign: "middle",
-    width: "75px",
-    cardVisible: false,
-    switchable: false,
-    formatter: function(value, row, index) {
-      return [
-        '<a class="zoom" href="javascript:void(0)" title="Zoom" style="margin-right: 10px;">',
-          '<i class="fa fa-search-plus"></i>',
-        '</a>',
-        '<a class="identify" href="javascript:void(0)" title="Identify" style="margin-right: 10px;">',
-          '<i class="fa fa-info-circle"></i>',
-        '</a>'
-      ].join("");
-    },
-    events: {
-      "click .zoom": function (e, value, row, index) {
-        var layer = featureLayer.getLayer(row.leaflet_stamp);
-        map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 19);
-        highlightLayer.clearLayers();
-        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
-      },
-      "click .identify": function (e, value, row, index) {
-        identifyFeature(row.leaflet_stamp);
-        highlightLayer.clearLayers();
-        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
-      }
-    }
-  }];
-
-  $.each(properties, function(index, value) {
-    // Filter config
-    if (value.filter) {
-      var id;
-      if (value.filter.type == "integer") {
-        id = "cast(properties->"+ value.value +" as int)";
-      }
-      else if (value.filter.type == "double") {
-        id = "cast(properties->"+ value.value +" as double)";
-      }
-      else {
-        id = "properties->" + value.value;
-      }
-      filters.push({
-        id: id,
-        label: value.label
-      });
-      $.each(value.filter, function(key, val) {
-        if (filters[index]) {
-          // If values array is empty, fetch all distinct values
-          if (key == "values" && val.length === 0) {
-            alasql("SELECT DISTINCT(properties->"+value.value+") AS field FROM ? ORDER BY field ASC", [geojson.features], function(results){
-              distinctValues = [];
-              $.each(results, function(index, value) {
-                distinctValues.push(value.field);
-              });
-            });
-            filters[index].values = distinctValues;
-          } else {
-            filters[index][key] = val;
-          }
-        }
-      });
-    }
-    // Table config
-    if (value.table) {
-      table.push({
-        field: value.value,
-        title: value.label
-      });
-      $.each(value.table, function(key, val) {
-        if (table[index+1]) {
-          table[index+1][key] = val;
-        }
-      });
-    }
-  });
-
-  buildFilters();
-  buildTable();
-};
-
-
-
 function buildFilters() {
   $("#query-builder").queryBuilder({
     allow_empty: true,
@@ -919,44 +920,45 @@ function applyFilter() {
 
 
 
-
-function buildTable() {
-  $("#table").bootstrapTable({
-    cache: false,
-    height: $("#table-container").height(),
-    undefinedText: "",
-    striped: false,
-    pagination: false,
-    minimumCountColumns: 1,
-    sortName: config.sortProperty,
-    sortOrder: config.sortOrder,
-    toolbar: "#toolbar",
-    search: true,
-    trimOnSearch: false,
-    showColumns: false,
-    showToggle: false,
-    columns: table,
-    onClickRow: function(row, $element) {
-      var layer = featureLayer.getLayer(row.leaflet_stamp);
-      map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 19);
-      highlightLayer.clearLayers();
-      highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
-    },
-    onDblClickRow: function(row) {
-      identifyFeature(row.leaflet_stamp);
-      highlightLayer.clearLayers();
-      highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
-    },
-  });
-
-  map.fitBounds(featureLayer.getBounds());
-
-  $(window).resize(function () {
-    $("#table").bootstrapTable("resetView", {
-      height: $("#table-container").height()
+if (userEmail != "") {
+  function buildTable() {
+    $("#table").bootstrapTable({
+      cache: false,
+      height: $("#table-container").height(),
+      undefinedText: "",
+      striped: false,
+      pagination: false,
+      minimumCountColumns: 1,
+      sortName: config.sortProperty,
+      sortOrder: config.sortOrder,
+      toolbar: "#toolbar",
+      search: true,
+      trimOnSearch: false,
+      showColumns: false,
+      showToggle: false,
+      columns: table,
+      onClickRow: function(row, $element) {
+        var layer = featureLayer.getLayer(row.leaflet_stamp);
+        map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 19);
+        highlightLayer.clearLayers();
+        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
+      },
+      onDblClickRow: function(row) {
+        identifyFeature(row.leaflet_stamp);
+        highlightLayer.clearLayers();
+        highlightLayer.addData(featureLayer.getLayer(row.leaflet_stamp).toGeoJSON());
+      },
     });
-  });
-};
+
+    map.fitBounds(featureLayer.getBounds());
+
+    $(window).resize(function () {
+      $("#table").bootstrapTable("resetView", {
+        height: $("#table-container").height()
+      });
+    });
+  };
+}
 
 
 
