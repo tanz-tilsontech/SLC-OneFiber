a7cde3da3e848141e06f35a1d7a5fb369e5660fb
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-06-06 08:04:22 -0600

    asdf

--- assets/js/tilson.js
+++ assets/js/tilson.js
@@ -1819,35 +1819,35 @@ var featureLayer2 = L.geoJson(null, {
         color: "green",
         weight: 6,
         opacity: 1,
-        dashArray: "30 10"
+        dashArray: "10"
       };
     } else if (feature.properties.oofstatus === "Construction Underway") {
       return {
         color: "magenta",
         weight: 6,
         opacity: 1,
-        dashArray: "30 10"
+        dashArray: "10"
       };
     } else if (feature.properties.oofstatus === "Permits Received") {
       return {
         color: "orange",
         weight: 6,
         opacity: 1,
-        dashArray: "30 10"
+        dashArray: "10"
       };
     } else if (feature.properties.oofstatus === "Permits Submitted") {
       return {
         color: "blue",
         weight: 6,
         opacity: 1,
-        dashArray: "30 10"
+        dashArray: "10"
       };
     } else if (feature.properties.oofstatus === "In Design") {
       return {
         color: "gray",
         weight: 6,
         opacity: 1,
-        dashArray: "30 10"
+        dashArray: "10"
       };
     }
   },
