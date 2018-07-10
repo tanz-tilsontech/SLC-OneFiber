8413fb45bf80242e91a840df682b6f3c6c5a68f5
tanz-tilsontech <36584403+tanz-tilsontech@users.noreply.github.com>
2018-07-09 20:12:55 -0600

    asdf

--- assets/js/tilson.js
+++ assets/js/tilson.js
@@ -73,7 +73,7 @@ var gisDemandPointsConfig = {
 // GIS VES2K CONFIG
 
 var gisVES2kConfig = {
-  geojson: "https://tilsonwebdraco.3-gislive.com/arcgis/rest/services/SLClld/Tilsonslc_lld/MapServer/49/query?where=objectid+IS+NOT+NULL&outFields=*&f=geojson",
+  geojson: "https://tilsonwebdraco.3-gislive.com/arcgis/rest/services/SLClld/Tilsonslc_lld/MapServer/49/query?where=objectid+IS+NOT+NULL+AND+nfid+IS+NOT+NULL&outFields=*&f=geojson",
   layerName: "GIS VES2k",
   hoverProperty: "site_name"
 };
